7170a48fe52ec3c9bef5b5bb91efb83a7f60f1cf
Franz Greiling <fgr@sadmac.de>
2018-03-16 17:11:43 +0100

    Fix ls colors on non-BSD systems

--- zsh/.zsh/lib/color.zsh
+++ zsh/.zsh/lib/color.zsh
@@ -1,14 +1,17 @@
 #
 # Color Settings
-# 
+#
 
 autoload colors && colors
 
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 export LSCOLORS='gxfxcxdxbxegedabagacad'
 
-# FIXME: Do this the platform independent way
-alias ls='ls -G'
+if (( $+commands[dircolors] )); then
+  alias ls='ls --color=auto'
+else
+  alias ls='ls -G'
+fi
 
 export GREP_COLOR='37;45'           # BSD.
 export GREP_COLORS="mt=$GREP_COLOR" # GNU.
