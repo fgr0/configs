841139603ac8ab8513169b05625a375390492010
Franz Greiling <fgr@sadmac.de>
2019-11-25 17:09:36 +0100

    zsh - add zsh-autosuggestions
    Adds new submodule, requires `git submodule update —init`

--- zsh/.zsh/lib/history.zsh
+++ zsh/.zsh/lib/history.zsh
@@ -11,8 +11,14 @@ setopt inc_append_history       # Write to the history file immediately
 setopt share_history            # Share history between all sessions
 
 setopt hist_ignore_dups         # Ignore duplicates of the previous event
-setopt hist_expire_dups_first   # Expire duplicates from history file first
 setopt hist_verify              # Verify history expansions before executing
 setopt hist_beep                # BOOP
 
 alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
+
+#
+# Setup history-based autosuggestions
+#
+
+source $ZDOTDIR/ext/zsh-autosuggestions/zsh-autosuggestions.zsh
